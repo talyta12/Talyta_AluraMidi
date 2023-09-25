861363e798c98f89c3b1a40bcc10d1566f4a441f
talyta12 <129244780+talyta12@users.noreply.github.com>
2023-09-25 19:51:34 -0300

    rgfgr

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function tocaSompom () {
 
 document.querySelector('.tecla_pom').onclick = tocaSompom;
 
+function tocaSomclap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
