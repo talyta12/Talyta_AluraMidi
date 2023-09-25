6368a10c7fa27e92038416546516b6e42c67a535
talyta12 <129244780+talyta12@users.noreply.github.com>
2023-09-25 19:58:45 -0300

    ghgh

--- main.js
+++ main.js
@@ -15,3 +15,40 @@ function tocaSomtim () {
 }
 
 document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
+function tocaSompuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSompuff;
+
+function tocaSomsplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomsplash;
+
+function tocaSomtoim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomtoim;
+
+function tocaSompsh() {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSompsh;
+
+function tocaSomtic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomtic;
+
+function tocaSomtom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomtom;
+
