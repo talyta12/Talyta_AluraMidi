a0839990808aaa285b222a72751b86b72d339e66
talytaaraujo <129244780+talyta12@users.noreply.github.com>
2023-10-09 19:53:00 -0300

    vygguyh7yh8

--- main.js
+++ main.js
@@ -1,54 +1,7 @@
+// Pom -------------
 function tocaSompom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSompom;
-
-function tocaSomclap () {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomclap;
-
-function tocaSomtim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomtim;
-
-function tocaSompuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSompuff;
-
-function tocaSomsplash () {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_splash').onclick = tocaSomsplash;
-
-function tocaSomtoim () {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-document.querySelector('.tecla_toim').onclick = tocaSomtoim;
-
-function tocaSompsh() {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-document.querySelector('.tecla_psh').onclick = tocaSompsh;
-
-function tocaSomtic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_tic').onclick = tocaSomtic;
-
-function tocaSomtom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_tom').onclick = tocaSomtom;
+ const listaDeTeclas = document.querySelectorAll('.tecla');
 
