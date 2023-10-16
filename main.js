dd9052f2671db776d41e620ed099e310380459a6
talytaaraujo <129244780+talyta12@users.noreply.github.com>
2023-10-16 19:50:56 -0300

    uuhijhub

--- main.js
+++ main.js
@@ -3,5 +3,16 @@ function tocaSompom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
- const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+\\enquanto
+while (contador = 9) {
+    listaDeTeclas[0].onclick = tocaSompom; 
+
+    contador = contador + 1;
+
+    console.log (contador);
+}
 
